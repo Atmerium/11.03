0876b55425dddb5b7bc317a768b03544779f3e7f
Attila Molnár <molnar.attila1@petrik.hu>
2025-11-06 21:27:15 +0100

    Minor Change

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -66,9 +66,8 @@ export class AppController {
       let datetimeLocal = body.datetime || '';
       try {
         const d = new Date(values.datetime);
-        if (!isNaN(d.getTime())) {
-          const pad = (n: number) => (n < 10 ? '0' + n : '' + n);
-          datetimeLocal = `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())}T${pad(d.getHours())}:${pad(d.getMinutes())}`;
+        if (!d) {
+          datetimeLocal = d
         }
       } catch {
         datetimeLocal = body.datetime || '';
